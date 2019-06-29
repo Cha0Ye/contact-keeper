7b5495233a914b7c8598ff50509b155c77bfa90f
Chao Ye <Chao12.ye@gmail.com>
2019-06-28 21:00:39 -0700

    Issue token to logged in user

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const router= express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const auth = require('../middleware/auth');
 const JWT_EXPIRATION= 360000;
 // validates inputs
 const { check, validationResult } = require('express-validator');
@@ -12,8 +13,15 @@ const User = require('../models/User');
 // @route     GET api/auth
 // @desc      Get logged in user
 // @access    Private
-router.get('/', (req, res) => {
-    res.send('Get logged in user');
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    }
+    catch(err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
 });
 
 
@@ -27,7 +35,7 @@ router.post('/', [
     async (req, res) => {
       const errors = validationResult(req);
 
-      // invalide credentials error
+      // invalid credentials error
       if(!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
       }
