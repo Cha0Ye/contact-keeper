cb960bd4027d4055355a09885a19fcfe1b51eafd
Chao Ye <Chao12.ye@gmail.com>
2019-06-30 19:40:19 -0700

    add contact filter search component ContactFilter

--- client/src/components/contacts/ContactFilter.js
+++ client/src/components/contacts/ContactFilter.js
@@ -1,12 +1,34 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef, useEffect } from 'react';
 import ContactContext from '../../context/contact/contactContext';
 
-function ContactFilter(props) {
-    const contactContext = useContext();
+const ContactFilter = () => {
+        const contactContext = useContext(ContactContext);
+        const text = useRef('');
+        const { filterContacts, clearFilter, filtered } = contactContext;
+    
+
+    useEffect(() => {
+        if(filtered === null) {
+            text.current.value = '';
+        }
+    });
+
+    const onChange = (e) => {
+        if(text.current.value !== '') {
+            filterContacts(e.target.value);
+        }
+        else {
+            clearFilter();
+        }
+    }
     return (
-        <div>
-            
-        </div>
+        <form>
+            <input ref={text} 
+                   type="text" 
+                   placeholder="Filter Contacts..." 
+                   onChange={onChange}
+            />
+        </form>
     );
 }
 export default ContactFilter;
