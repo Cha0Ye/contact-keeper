de9f99805a037f445a62bad5cc63ce80119f8149
Chao Ye <Chao12.ye@gmail.com>
2019-07-03 13:15:55 -0700

    add required to add contact form

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -58,18 +58,21 @@ const ContactForm = () => {
                    name="name" 
                    value={name} 
                    onChange={onChange}
+                   required
             />
             <input type="text" 
                    placeholder="Email" 
                    name="email" 
                    value={email} 
                    onChange={onChange}
+                   required
             />
             <input type="text" 
                    placeholder="Phone" 
                    name="phone" 
                    value={phone} 
                    onChange={onChange}
+                   required
             />
             <h5>Contact Type</h5>
 
