d319f02eb357d4d6d882ae2a4eae3b4271bfcdee
Chao Ye <Chao12.ye@gmail.com>
2019-07-03 12:58:40 -0700

    get user contacts and display

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -18,7 +18,7 @@ import {
 
 const ContactState = props => {
     const initialState = {
-        contacts: [],
+       contacts: null,
        current: null,
        filtered: null,
        error: null
@@ -119,7 +119,8 @@ const ContactState = props => {
             clearCurrent,
             updateContact,
             filterContacts, 
-            clearFilter
+            clearFilter,
+            getContacts
           }}>
         
         { props.children } 
--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -21,13 +21,18 @@ export default (state, action) => {
             }
         case ADD_CONTACT:
             return {
-                ...state, contacts: [...state.contacts, action.payload]
+                ...state, 
+                contacts: [...state.contacts, action.payload],
+                loading: false
             };
         
         case UPDATE_CONTACT:
                 return {
                 ...state, 
-                contacts: state.contacts.map(contact => contact.id === action.payload.id ? action.payload : contact)
+                contacts: state.contacts.map(contact => 
+                    contact.id === action.payload.id ? action.payload : contact
+                    ),
+                loading: false
             };
         
         case DELETE_CONTACT:
